ef21d9b903c4c096a62443399fd88fd9859b3ecf
Maxn_Rain <wangbingzhe020315@gmail.com>
2022-09-30 19:45:58 +0800

    1.0.60

--- src/App.vue
+++ src/App.vue
@@ -23,9 +23,34 @@ html,
 <script>
 export default {
   name: "App",
-  methods: {},
+  methods: {
+     handleScreen(){
+        const m = this.detectZoom();
+        console.log(m, 'MMMMMMMMMMMMMMM')
+        document.body.style.zoom = 100/Number(m);
+      },
+      detectZoom () {
+		      let ratio = 0,
+		        screen = window.screen,
+		        ua = navigator.userAgent.toLowerCase();
+		      if (window.devicePixelRatio !== undefined) {
+		        ratio = window.devicePixelRatio;
+		      } else if (~ua.indexOf('msie')) {
+		        if (screen.deviceXDPI && screen.logicalXDPI) {
+		          ratio = screen.deviceXDPI / screen.logicalXDPI;
+		        }
+		      } else if (window.outerWidth !== undefined && window.innerWidth !== undefined) {
+		        ratio = window.outerWidth / window.innerWidth;
+		      }
+		      if (ratio) {
+		        ratio = Math.round(ratio * 100);
+		      }
+		      return ratio;
+		  }
+
+  },
   mounted: function () {
-    window.scrollTo((document.body.scrollWidth-document.body.offsetWidth)/2,0)
+   this.handleScreen();
   },
 };
 </script>
