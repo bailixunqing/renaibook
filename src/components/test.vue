ca5a4cf3500398dfae98249428cfe350c57667d6
Maxn_Rain <wangbingzhe020315@gmail.com>
2022-10-07 14:46:28 +0800

    1.0.63

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="screen">
     <div class="background">
-        <img src="../assets/images/index.png" />
+
       </div>
 
 
@@ -37,8 +37,11 @@ export default {
 .screen{
   display: flex;
   justify-content: center;
-  background-color: #000;
+    background-image: linear-gradient(90deg,rgba(0, 47, 255, 0.1) 0,rgba(131, 26, 5, 0.1) 15%,rgba(167, 55, 87, 0.1) 30%,rgba(161,102,171,.1) 44%,rgba(80,115,184,.1) 58%,rgba(16,152,173,.1) 72%,rgba(7,179,155,.1) 86%,rgba(109,186,130,.1) 100%);
   width: 100%;
   height:100%;
+}
+.background{
+  /* background-image: linear-gradient(90deg,rgba(247,149,51,.1) 0,rgba(243,112,85,.1) 15%,rgba(239,78,123,.1) 30%,rgba(161,102,171,.1) 44%,rgba(80,115,184,.1) 58%,rgba(16,152,173,.1) 72%,rgba(7,179,155,.1) 86%,rgba(109,186,130,.1) 100%); */
 }
 </style>
