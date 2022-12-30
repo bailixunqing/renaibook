0dfd038f2c004c58431c40008ee617a3f29d7d2b
jihuangxie@dingtalk.com <jihuangxie@dingtalk.com>
2022-12-30 12:28:01 +0800

    优化

--- src/components/home.vue
+++ src/components/home.vue
@@ -918,14 +918,10 @@ this.$router.push({
   flex-flow: column;
   z-index: -999;
   align-items: center;
-
   margin-top: 20px;
-  width: 1000px;
-  height: 300px;
   background: rgba(255, 255, 255, 0.6);
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   backdrop-filter: blur(10px);
-
   border-radius: 28px;
 }
 
