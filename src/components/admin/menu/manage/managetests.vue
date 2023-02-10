2b46e95ddb367758b170b2848a30809dbf3fda46
jihuangxie@dingtalk.com <jihuangxie@dingtalk.com>
2023-02-10 11:28:15 +0800

    添加拖拽效果

--- src/components/admin/menu/manage/managetests.vue
+++ src/components/admin/menu/manage/managetests.vue
@@ -2,7 +2,8 @@
   <div class="menu">
     <div class="menu_body">
       <el-form-item label="编辑目录">
-        <a-tree v-model:expandedKeys="expandedKeys" :tree-data="treeData"  :show-line="showLine">
+        <a-tree v-model:expandedKeys="expandedKeys" draggable :tree-data="gData" :show-line="showLine"
+          @dragenter="onDragEnter" @drop="onDrop">
           <template #title="{ key: treeKey, title }">
             <a-dropdown :trigger="['contextmenu']">
               <span>{{ title }}</span>
@@ -21,7 +22,9 @@
 </template>
 <script setup>
 import { watch, ref } from 'vue';
+/* 是否展示连接线 */
 const showLine = ref(true);
+/* 数据 */
 const treeData = [{
   title: '馆藏资源',
   key: '0-0-0',
@@ -36,20 +39,92 @@ const treeData = [{
   title: '馆藏寄语',
   key: '0-0-1',
   children: [{
-    title: '馆藏寄语1',
+    title: '馆藏寄语3',
     key: '0-0-1-0',
   }, {
-    title: '馆藏寄语2',
+    title: '馆藏寄语4',
     key: '0-0-1-1',
   }],
 }];
+const gData = ref(treeData);
+/*  右键 */
 const onContextMenuClick = (treeKey, menuKey) => {
   console.log(`treeKey: ${treeKey}, menuKey: ${menuKey}`);
 };
+/* 控制  */
 const expandedKeys = ref(['0-0-0', '0-0-1']);
 watch(expandedKeys, () => {
   console.log('expandedKeys', expandedKeys);
 });
+
+/* 以下属于滑动效果部分 */
+/**
+ *  draggable  节点可拖拽
+ * 
+ */
+/* 事件2 */
+const onDragEnter = info => {
+  console.log(info);
+  // expandedKeys 需要展开时
+  // expandedKeys.value = info.expandedKeys;
+};
+/* 事件1 */
+const onDrop = info => {
+  console.log(info);
+  const dropKey = info.node.key;
+  const dragKey = info.dragNode.key;
+  const dropPos = info.node.pos.split('-');
+  const dropPosition = info.dropPosition - Number(dropPos[dropPos.length - 1]);
+  const loop = (data, key, callback) => {
+    data.forEach((item, index) => {
+      if (item.key === key) {
+        return callback(item, index, data);
+      }
+      if (item.children) {
+        return loop(item.children, key, callback);
+      }
+    });
+  };
+  const data = [...gData.value];
+  // Find dragObject
+  let dragObj;
+  loop(data, dragKey, (item, index, arr) => {
+    arr.splice(index, 1);
+    dragObj = item;
+  });
+  if (!info.dropToGap) {
+    // Drop on the content
+    loop(data, dropKey, item => {
+      item.children = item.children || [];
+      /// where to insert 示例添加到头部，可以是随意位置
+      item.children.unshift(dragObj);
+    });
+  } else if ((info.node.children || []).length > 0 &&
+    // Has children
+    info.node.expanded &&
+    // Is expanded
+    dropPosition === 1 // On the bottom gap
+  ) {
+    loop(data, dropKey, item => {
+      item.children = item.children || [];
+      // where to insert 示例添加到头部，可以是随意位置
+      item.children.unshift(dragObj);
+    });
+  } else {
+    let ar = [];
+    let i = 0;
+    loop(data, dropKey, (_item, index, arr) => {
+      ar = arr;
+      i = index;
+    });
+    if (dropPosition === -1) {
+      ar.splice(i, 0, dragObj);
+    } else {
+      ar.splice(i + 1, 0, dragObj);
+    }
+  }
+  gData.value = data;
+};
 </script>
 <style>
 .menu {
